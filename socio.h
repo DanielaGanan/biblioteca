3ca612ba08881cd22e878a2d2df05479c86bac92
Matias Vergara <matias.vergara@tsoftlatam.com>
2024-11-07 23:09:45 -0300

    Se modifica el .gitignore

--- socio.h
+++ socio.h
@@ -2,6 +2,7 @@
 #define SOCIO_H
 
 #include <QWidget>
+#include "Persona.h"
 
 namespace Ui {
 class Socio;
@@ -15,8 +16,22 @@ public:
     explicit Socio(QWidget *parent = nullptr);
     ~Socio();
 
+private slots:
+    bool consultarEstado();
+
 private:
     Ui::Socio *ui;
+    Persona persona; // Composición para tener disponibles los métodos de Persona
+    int idSocio;
+    bool estado;
+
+    //Getters
+    int getIdSocio() const;
+    bool getEstado() const;
+
+    //Setters
+    void setIdSocio(const int idSocio);
+    void setEstado(const bool estado);
 };
 
 #endif // SOCIO_H
