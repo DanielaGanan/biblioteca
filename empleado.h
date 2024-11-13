53a8ae7638f8c3c10cdb745a3c1eca11498e0b93
Matias Vergara <matias.vergara@tsoftlatam.com>
2024-11-12 21:05:26 -0300

    Se limpian las clases

--- empleado.h
+++ empleado.h
@@ -16,23 +16,10 @@ public:
     explicit Empleado(QWidget *parent = nullptr);
     ~Empleado();
 
-    int getIdEmpleado();
-    QString getUsuario();
-    QString getContrasenia();
-    void setIdEmpleado(int idEmpleado);
-    void setUsuario(QString usuario);
-    void setContrasenia(QString contrasenia);
-
 private slots:
-    bool agregarEmpleado();
-    bool eliminarEmpleado();
-    bool modificarEmpleado();
 
 private:
     Ui::Empleado *ui;
-    int idEmpleado;
-    QString usuario;
-    QString contrasenia;
 };
 
 #endif // EMPLEADO_H
--- prestamo.h
+++ prestamo.h
@@ -3,9 +3,6 @@
 
 #include <QWidget>
 #include "QDebug"
-#include "libro.h"
-#include "socio.h"
-#include "empleado.h"
 #include <QDate>
 
 namespace Ui {
@@ -20,39 +17,11 @@ public:
     explicit Prestamo(QWidget *parent = nullptr);
     ~Prestamo();
 
-    int getIdPrestamo();
-    Libro getLibro();
-    Socio getSocio();
-    int getCantidad();
-    Empleado getEmpleado();
-    QDate getFechaPrestamo();
-    QDate getFechaDevolucion();
-    void setIdPrestamo(int idPrestamo);
-    void setLibro(Libro libro);
-    void setSocio(Socio socio);
-    void setCantidad(int cantidad);
-    void setEmpleado(Empleado empledo);
-    void setFechaPrestamo(QDate fechaPrestamo);
-    void setFechaDevolucion(QDate fechaDevolucion);
-
 private slots:
-  //  void fecha(QDate fecha); // Busca los libros en cierta fecha?
-  //  void fechaDevolucion(Qdate fecha); // ?
-    vector buscarPrestamoSocio(Socio socio); // Para buscar los prestamos a un socio
- //   void cantidad(int cantidad);
-    bool solicitarLibro();  //Agregar o solicitar un prestamo
-    bool devolverLibro();   //Devuelve un libro o libros
-    bool modificarPrestamo(); // Para modificar un prestamo
+
 
 private:
     Ui::Prestamo *ui;
-    int idPrestamo;
-    Libro libro;
-    Socio socio;
-    int cantidad;
-    Empleado empleado;
-    QDate fechaPrestamo;
-    QDate fechaDevolucion;
 
 };
 
